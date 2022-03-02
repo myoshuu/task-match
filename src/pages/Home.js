88c78475d84c23d3491898ed03bc4c36cc317843
Erik-Sanjaya <erik.6928@ski.sch.id>
2022-03-03 05:05:34 +0700

    Add Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,53 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          height: "100vh",
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          justifyItems: "center",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="col" style={{}}>
+          <h1>Joevano</h1>
+          <h1>8</h1>
+          <h2>Tasks Completed This Week</h2>
+          <h3>Week 1</h3>
+          <ul style={{ listStyle: "none" }}>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+          </ul>
+        </div>
+        <div className="col">
+          <h1>Joevano</h1>
+          <h1>8</h1>
+          <h2>Tasks Completed This Week</h2>
+          <h3>Week 1</h3>
+          <ul>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+            <li>TJBL - Express</li>
+          </ul>
+        </div>
+      </div>
+      <div className="main" style={{ height: "100vh" }}>
+        Test
+      </div>
+    </>
+  );
 };
 
 export default Home;
